683f598cd537f2658db8eaccb1e42caf759b18bf
jeddstumpf <jstumpf@stmesc.org>
2024-11-20 21:18:57 +0000

    func and varb

--- lessons/00_Turtles/05c_Loop_with_Turtle.py
+++ lessons/00_Turtles/05c_Loop_with_Turtle.py
@@ -7,15 +7,21 @@ write a new program that uses a loop to draw a pentagon.
 ( You can cut and past most of it! )
 
 """
+import turtle                           # Tell Python we want to work with the turtle
+turtle.setup(width=600, height=600)     # Set the size of the window
+
+tina = turtle.Turtle()                  # Create a turtle named tina
+
+tina.shape('turtle')                    # Set the shape of the turtle to a turtle
+tina.speed(2)  
 
 ... # Your code here
-
-
-
-
-
-
-
+sides=5
+angle=360/sides
+for i in range(sides):
+    tina.forward(50)
+    tina.left(angle)
+turtle.exitonclick()
 
 
 

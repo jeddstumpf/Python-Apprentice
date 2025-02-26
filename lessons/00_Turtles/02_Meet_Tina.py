6cfaa5a55ef36056c93dbcf2f93fc300e1b31d28
jeddstumpf <jstumpf@stmesc.org>
2025-02-26 21:46:57 +0000

    housestealer

--- lessons/00_Turtles/02_Meet_Tina.py
+++ lessons/00_Turtles/02_Meet_Tina.py
@@ -9,46 +9,55 @@ RUM ME! YOu can run this program by clicking on ▶️ icon ar the top of the ed
 window.
 
 """
+import random
+import turtle
 
-import turtle                           # Tell Python we want to work with the turtle
+
+
+
+                          # Tell Python we want to work with the turtle
 turtle.setup(width=600, height=600)     # Set the size of the window
 
 tina = turtle.Turtle()                  # Create a turtle named tina
 
-tina.shape('turtle')                    # Set the shape of the turtle to a turtle
-tina.speed(2)                           # Make the turtle move as fast, but not too fast.
-
-tina.forward(190)
-tina.left(160)
-tina.forward(190)
-tina.left(25)
-tina.left(25)
-tina.forward(130)
-tina.left(150)
-tina.forward(110)
-tina.left(180)
-tina.forward(100)
-tina.left(90)
-tina.forward(150)
-tina.left(90)
-tina.forward(275)
-tina.left(90)
-tina.forward(150)
-tina.left(55)
-tina.forward(110)
-tina.right(80)
-tina.forward(110)
-tina.right(60)
-tina.forward(90)
-tina.right(70)
-tina.forward(145)
-tina.right(90)
-tina.forward(100)
 
 
 
-
-
+for i in range (4):
+    x = random.randint(-200,200)
+    y = random.randint(-200,200)
+    tina.shape('circle')                    # Set the shape of the turtle to a turtle
+    tina.speed(3)                           # Make the turtle move as fast, but not too fast.
+    tina.color("teal")
+    tina.forward(190)
+    tina.left(160)
+    tina.forward(190)
+    tina.left(25)
+    tina.left(25)
+    tina.forward(130)
+    tina.left(150)
+    tina.forward(100)
+    tina.left(180)
+    tina.forward(100)
+    tina.color("blue")
+    tina.left(90)
+    tina.forward(170)
+    tina.left(90)
+    tina.forward(290)
+    tina.left(90)
+    tina.forward(175)
+    tina.left(75)
+    tina.forward(100)
+    tina.right(80)
+    tina.color("brown")
+    tina.forward(130)
+    tina.right(85)
+    tina.forward(85)
+    tina.right(90)
+    tina.forward(155)
+    tina.penup()
+    tina.goto(x,y)
+    tina.pendown()
 
 
 
